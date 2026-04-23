e82a9ea668af08cb6d1127342524720a3e674fc3
Elly Onunga <onungaelly@gmail.com>
2026-04-23 14:48:32 +0300

    managed the secrets

--- pure-test.cjs
+++ pure-test.cjs
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 
 async function main() {
-  const uriSrv = "mongodb://elly:elly@ac-d862qfj-shard-00-00.vvy07rk.mongodb.net:27017,ac-d862qfj-shard-00-01.vvy07rk.mongodb.net:27017,ac-d862qfj-shard-00-02.vvy07rk.mongodb.net:27017/quty?tls=true&replicaSet=atlas-2y4p5q-shard-0&authSource=admin&retryWrites=true&w=majority";
+  const uriSrv = "DATABASE_URL";
   console.log("Testing DIRECT URL with IPv4 forced:", uriSrv);
   
   const client = new MongoClient(uriSrv, {
